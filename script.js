4c0e1ce6f3e9aa3661e8aee0d2565a27b54c8652
john <john.muchiri@student.moringaschool.com>
2024-07-22 12:17:04 +0300

    Refactor movie listing and state management

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-// Globals
+// A: Globals
 // 1. Constants
 const API_URL = "http://localhost:3000";
 const endpoint = "films";
 
 // 2. State
-let firstMovieData = undefined;
+const state = {
+  firstMovieData: undefined,
+  movieListData: [],
+};
+const stateKeys = {
+  firstMovieData: "firstMovieData",
+  movieListData: "movieListData",
+};
+
+// 3. State update functions
+function setState(name, newState) {
+  state[name] = newState;
+}
+
+function getState(name) {
+  return state[stateKeys[name]];
+}
 
 // 3. DOM elements
 const firstMovieCardEl = document.querySelector(".first-movie div.movie");
+const movieListEl = document.querySelector(".movie-list");
 
 // Steps to get the app working
 showFirstMovieDetails();
+showMovieMenu();
 
-// Helper functions
+// B: Helper functions
 async function showFirstMovieDetails() {
   // Get movie details from API
   const movieData = await getMovie(1);
-  // Add movie data to state
-  firstMovieData = movieData;
+  // Update firstMovieData state
+  setState(stateKeys.firstMovieData, movieData);
   // Render new state data
-  renderFirstMovie(firstMovieData);
+  renderFirstMovie(getState("firstMovieData"));
 }
 
 async function getMovie(movieId) {
@@ -57,4 +75,67 @@ function renderFirstMovie(data) {
 
   const availableTickets = data.capacity - data.tickets_sold;
   firstMovieCardAvailableTickets.textContent = `Available Tickets: ${availableTickets}`;
+}
+
+async function showMovieMenu() {
+  // Get movie menu data
+  const movieListData = await getMovies();
+  // Update state
+  setState(stateKeys.movieListData, movieListData);
+  // Render movie menu
+  renderMovieMenu(getState(stateKeys.movieListData));
+}
+
+async function getMovies() {
+  // Make GET request to API
+  const moviesListData = await fetchMovies();
+  return moviesListData;
+}
+
+async function fetchMovies() {
+  try {
+    const response = await fetch(`${API_URL}/${endpoint}`);
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok (${response.status})`);
+    }
+    const data = await response.json(); // Parse the data as JSON
+    return data;
+  } catch (error) {
+    console.error(`There was a problem with your fetch operation: ${error}`);
+  }
+}
+
+function renderMovieMenu(data) {
+  const fragmentEl = document.createDocumentFragment();
+  // Create menu item for each element in array
+  const menuIitemListEl = data.map((movieData) => createMenuItemEl(movieData));
+  // Clear movie menu html element in preparation for new render
+  clearMovieMenu();
+  // Display new data
+  for (const menuItemEl of menuIitemListEl) {
+    fragmentEl.appendChild(menuItemEl);
+  }
+  movieListEl.appendChild(fragmentEl);
+}
+
+function createMenuItemEl(movieData) {
+  const movieEl = document.createElement("li");
+  movieEl.classList.add("movie");
+
+  const moviePosterEl = document.createElement("img");
+  moviePosterEl.setAttribute("src", movieData.poster);
+  moviePosterEl.classList.add("poster");
+  movieEl.appendChild(moviePosterEl);
+
+  const movieTitleEl = document.createElement("h5");
+  movieTitleEl.classList.add("title");
+  movieTitleEl.textContent = movieData.title;
+  movieEl.appendChild(movieTitleEl);
+
+  return movieEl;
+}
+
+function clearMovieMenu() {
+  movieListEl.innerHTML = null;
 }
